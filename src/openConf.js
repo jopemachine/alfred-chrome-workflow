52118aed66a3d5bab37a993fc206780e86d43e0b
jopemachine <jopemachine@naver.com>
2021-04-02 01:27:12 +0900

    Update to 0.2.8

--- src/openConf.js
+++ src/openConf.js
@@ -1,7 +1,12 @@
 const alfy = require('alfy');
+const fs = require('fs');
 
 (async function () {
   const path = alfy.config.path;
+  if (!fs.existsSync(path)) {
+    require('./init.js');
+  }
+
   alfy.output([{ 
     title: 'Open config file',
     arg: path 
